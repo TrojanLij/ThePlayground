2ecd4a91015402010a0e1af9be39690782bfa53f
Lij <1sk8rdude@gmail.com>
2019-02-24 15:55:54 +0100

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#
+# MIT License
+#
+# Copyright (c) 2019 TrojanLij <https://github.com/TrojanLij>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+sudo apt-get install lsb-release &&
+sudo apt-get install figlet &&
+sudo apt-get install update-motd &&
+sudo rm -R /etc/update-motd.d/ &&
+sudo mkdir /etc/update-motd.d/ &&
+sudo touch /etc/update-motd.d/00-header ; sudo touch /etc/update-motd.d/10-sysinfo ; sudo touch /etc/update-motd.d/90-footer &&
+sudo chmod +x /etc/update-motd.d/* &&
+sudo curl -L https://goo.gl/N7btgM > /etc/update-motd.d/00-header &&
+sudo curl -L https://goo.gl/Z4zKtJ > /etc/update-motd.d/10-sysinfo &&
+sudo curl -L https://goo.gl/tYuRK9 > /etc/update-motd.d/90-footer
